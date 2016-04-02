5793485180b6b0b72c17363928841e15a66619ad
Johna N. Morris <johna.n.morris@gmail.com>
2016-04-01 22:33:28 -0700

    Modify style.css, index.html, helper.js, and resumeBuilder.js; delete fry.jpg and 197x148.jpg; added header.png, project01.png, project02.png, and project03.png

--- js/helper.js
+++ js/helper.js
@@ -8,12 +8,14 @@ Cameron Pittman
 */
 
 
-/*
+/*$(document).click(function(loc) {
+  // your code goes here
+});
 These are HTML strings. As part of the course, you'll be using JavaScript functions
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span id ="role">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
@@ -27,37 +29,44 @@ var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskills = '<li class="flex-item"><span class="skills-text">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
-var HTMLworkEmployer = '<a href="#">%data%';
-var HTMLworkTitle = ' - %data%</a>';
+var HTMLworkEmployer = '<h4>%data%';
+var HTMLworkTitle = ' &mdash; %data%</h4>';
 var HTMLworkDates = '<div class="date-text">%data%</div>';
 var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<h4>%data%</h4>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
-var HTMLschoolName = '<a href="#">%data%';
-var HTMLschoolDegree = ' -- %data%</a>';
+var HTMLschoolName = '<h4>%data%';
+var HTMLschoolDegree = ' &mdash; %data%</h4>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<p><br/>Major: %data%</p>';
+var HTMLschoolURL = '<a href="%data%">%data%</a>';
 
 var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
-var HTMLonlineSchool = ' - %data%</a>';
+var HTMLonlineSchool = '<h4>%data%</h4>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
-var HTMLonlineURL = '<br><a href="#">%data%</a>';
+var HTMLonlineTitle = '<p><br/>%data%</p>';
+var HTMLonlineURL = '<a href="%data%">%data%</a>';
+
+var HTMLotherClasses = '<h3>Other Courses</h3>';
+var HTMLotherTitle = '<p><br/>%data%</p>';
+var HTMLotherSchool = '<h4>%data%</h4>';
+var HTMLotherDates = '<div class="date-text">%data%</div>';
+var HTMLotherLocation = '<div class="location-text">%data%</div>';
+var HTMLotherURL = '<a href="%data%">%data%</a>';
 
 var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
-
 
 /*
 The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
@@ -65,7 +74,7 @@ The International Name challenge in Lesson 2 where you'll create a function that
 $(document).ready(function() {
   $('button').click(function() {
     var iName = inName() || function(){};
-    $('#name').html(iName);  
+    $('#name').html(iName);
   });
 });
 
@@ -85,9 +94,11 @@ function logClicks(x,y) {
 }
 
 $(document).click(function(loc) {
-  // your code goes here!
-});
+  var x = loc.pageX;
+  var y = loc.pageY;
 
+  logClicks(x,y);
+});
 
 
 /*
@@ -98,74 +109,63 @@ https://developers.google.com/maps/documentation/javascript/reference
 var map;    // declares a global map variable
 
 
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
+    var locations;
+    var mapOptions = {
+        disableDefaultUI: true
+    };
 
-  var locations;
+    /*
+    For the map to be displayed, the googleMap var must be
+    appended to #mapDiv in resumeBuilder.js.
+    */
+    map = new google.maps.Map(document.querySelector('#map'), mapOptions);
 
-  var mapOptions = {
-    disableDefaultUI: true
-  };
-
-  /* 
-  For the map to be displayed, the googleMap var must be
-  appended to #mapDiv in resumeBuilder.js. 
-  */
-  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
-
-
-  /*
-  locationFinder() returns an array of every location string from the JSONs
-  written for bio, education, and work.
-  */
-  function locationFinder() {
-
+    /*
+    locationFinder() returns an array of every location string from the JSONs
+    written for bio, education, and work.
+    */
+    function locationFinder()
+    {
     // initializes an empty array
     var locations = [];
-
     // adds the single location property from bio to the locations array
     locations.push(bio.contacts.location);
-
     // iterates through school locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
-    education.schools.forEach(function(school){
-      locations.push(school.location);
-    });
-
+    education.schools.forEach(function(school){locations.push(school.location);});
+    education.otherCourses.forEach(function(course){locations.push(course.location);});
     // iterates through work locations and appends each location to
     // the locations array. Note that forEach is used for array iteration
-    // as described in the Udacity FEND Style Guide: 
+    // as described in the Udacity FEND Style Guide:
     // https://udacity.github.io/frontend-nanodegree-styleguide/javascript.html#for-in-loop
     work.jobs.forEach(function(job){
-      locations.push(job.location);
+        locations.push(job.location);
     });
 
     return locations;
-  }
+    }
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat();  // latitude from the place service
+        var lon = placeData.geometry.location.lng();  // longitude from the place service
+        var name = placeData.formatted_address;   // name of the place from the place service
+        var bounds = window.mapBounds;            // current boundaries of the map window
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.formatted_address;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
-
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name
-    });
+        // marker is an object with additional data about the pin for a single location
+        var marker = new google.maps.Marker({
+            map: map,
+            position: placeData.geometry.location,
+            title: name
+        });
 
     // infoWindows are the little helper windows that open when you click
     // or hover over a pin on a map. They usually contain more information
@@ -176,7 +176,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -238,11 +238,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
+window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+map.fitBounds(mapBounds);
+});
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,274 @@
+var bio = {
+	"name" : "Johna N. Morris",
+	"role" : "Front-End Developer",
+	"welcomeMessage" : "Hello! ~ Bonjour! ~ Buongiorno!",
+	"contacts": {
+		"mobile": "(702) 406-4349",
+		"email" : "johna.n.morris@gmail.com",
+		"twitter": "@ParolePerdute",
+		"github" : "johnanmorris",
+		"location" : "Kent, WA"
+	},
+	"skills" : [
+		"HTML/CSS",
+		"Python",
+		"JavaScript",
+		"French",
+		"Italian"],
+}
+
 /*
-This is empty on purpose! Your code to build the resume will go here.
- */
+Iterates through the bio.skills array to produce a new formattedSkill
+variable to append to the #skills id
+Formats helper.js HTML strings from data in bio variable, and prepends
+them to the #header id.
+Iterates through bio.contacts to produce a new formattedContact
+and appends it to the #footerContacts id.
+*/
+
+bio.display = function ()
+{
+	$("#header").prepend(HTMLskillsStart);
+	for (var i = 0, len = bio.skills.length; i < len; i++)
+	{
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(formattedSkill);
+	}
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").prepend(formattedName + formattedRole + formattedWelcomeMsg);
+	for (contact in bio.contacts)
+	{
+		if (bio.contacts.hasOwnProperty(contact))
+		{
+			var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contacts[contact]);
+			formattedContact = formattedContact.replace("%contact%", contact);
+			$("#footerContacts").append(formattedContact);
+		}
+	}
+}
+
+bio.display();
+
+var work = {
+	"jobs" : [
+		{
+			"employer": "iSoftstone",
+			"title": "Search Engine Evaluator (Independent Contractor)",
+			"location": "Kirkland, WA",
+			"dates": "May 2013-Present",
+			"description": "Evaluate Bing search results for relevance; Complete approx. 80&ndash;100 query judgments per hour"
+		},
+		{
+			"employer": "Corsco, Inc. (Mrs. Clean)",
+			"title": "Editor",
+			"location": "Mill Creek, WA",
+			"dates": "April 2014-July 2015",
+			"description": "Edited the content of three websites for grammar, style, clarity, factual accuracy, &amp; HTML meta data; Rewrote articles and blog posts with significant grammar issues to increase readability; Reviewed 400+ articles for plagiarism and completed an assessment of plagiarism levels; Interacted with website users through forums, Facebook, and other social media"
+		},
+		{
+			"employer": "Klamath National Forest (Student Conservation Association)",
+			"title": "Archaeology Intern",
+			"location": "Happy Camp, CA",
+			"dates": "June 2012-August 2012",
+			"description": "Recorded and re-recorded historic mining sites using State of California preservation forms, and contributed to survey reports at the completion of each project; Assessed archaeological sites accurately and with minimal supervision; Used digital cameras, GPS units, and other specialized tools to accurately record archaeological sites"
+		},
+		{
+			"employer": "Ricoh USA (Amazon Campus)",
+			"title": "Mailroom Services Representative",
+			"location": "Seattle, WA",
+			"dates": "March 2012-May 2012",
+			"description": "Provided Amazon employees with excellent customer service regarding purchasing of shipping services and mail products; Prepared mail, UPS parcels, and DHL parcels for outbound delivery"
+		},
+		{
+			"employer": "Las Vegas-Clark County Library District",
+			"title": "Mailroom Clerk",
+			"location": "Las Vegas, NV",
+			"dates": "October 2011-November 2011",
+			"description": "Received and distributed UPS, FedEx, and US postalmail, and prepared outgoing mail for delivery"
+		},
+		{
+			"employer": "Vannah &amp; Vannah",
+			"title": "Legal Assistant",
+			"location": "Las Vegas, NV",
+			"dates": "July 2009-June 2011",
+			"description": "Maintained and updated medical records for around 30&ndash;50 clients; Organized medical records and calculated total medical costs for each client; Drafted professional correspondence to medical providers to request documents; Assembled documents for discovery"
+		},
+		{
+			"employer": "University of Nevada Library: E-Reserves &amp; Document Delivery",
+			"title": "Library Assistant",
+			"location": "Reno, NV",
+			"dates": "August 2006-May 2009",
+			"description": "Scanned books and documents, then edited them in Adobe Acrobat (cropping, OCR); Entered bibliographic data for scanned articles and book excerpts into library&rsquo;s electronic reserves database"
+		}
+	]
+}
+
+/*
+Iterates through work.jobs to create a new string from helper.js, and
+appends each to a new div of the work-entry class
+*/
+
+work.display = function()
+{
+	for (job in work.jobs)
+	{
+		if (work.jobs.hasOwnProperty(job))
+		{
+			$("#workExperience").append(HTMLworkStart);
+			var formattedWorkEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+			var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+			var formattedWorkLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+			var formattedWorkDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+			var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+			$(".work-entry:last").append(formattedWorkEmployer + formattedWorkTitle + formattedWorkLocation +
+				formattedWorkDates + formattedWorkDescription);
+		}
+	}
+}
+
+work.display();
+
+var projects = {
+	"projects": [
+		{
+			"title": "Udacity Stage 1: Make a Web Page",
+			"dates": "May 2015",
+			"description": "Basic HTML/CSS website for keeping Udacity Class Notes.",
+			"images": ["images/project01.png"]
+		},
+		{
+			"title": "Udacity Stage 2: Code Your Own Quiz",
+			"dates": "October 2015",
+			"description": "A Python program to run a fill-in-the-blanks quiz.",
+			"images": ["images/project02.png"]
+		},
+		{
+			"title": "Udacity Stage 3: Create a Movie Website",
+			"dates": "November 2015",
+			"description": "A movies website generated by Python, using classes.",
+			"images": ["images/project03.png"]
+		}
+	]
+}
+
+/*
+Iterates through projects.projects to create a new string from helper.js,
+and appends each to a new div of the project-entry class
+Additionally, if the projects.projects.images array is not empy, formats
+the information into an <img> tag and appends it to the project-entry class
+*/
+
+projects.display = function()
+	{
+		for (project in projects.projects)
+		{
+			if (projects.projects.hasOwnProperty(project))
+			{
+				$("#projects").append(HTMLprojectStart);
+				var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+				$(".project-entry:last").append(formattedProjectTitle);
+				var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+				$(".project-entry:last").append(formattedProjectDates);
+				var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+				$(".project-entry:last").append(formattedProjectDescription);
+				if (projects.projects[project].images.length > 0)
+				{
+					var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images);
+					$(".project-entry:last").append(formattedProjectImage);
+				}
+			}
+		}
+	}
+
+projects.display()
+
+var education = {
+	"schools": [
+		{
+			"name": "University of Nevada",
+			"location": "Reno, NV",
+			"degree": "Bachelor of Arts",
+			"majors": "Anthropology, Linguistics (minor)",
+			"dates": "2006-2009",
+			"url" : "https://www.unr.edu"
+		},
+		{
+			"name": "Community College of Southern Nevada",
+			"location": "Las Vegas, NV",
+			"degree": "transfer",
+			"majors": "Anthropology",
+			"dates": "2004-2006",
+			"url" : "https://www.csn.edu"
+		}
+	],
+
+	"onlineCourses" : [
+		{
+			"title" : "Intro to Programming Nanodegree",
+			"school" : "Udacity",
+			"dates" : "May 2015-April 2016",
+			"url" : "https://www.udacity.com/course/intro-to-programming-nanodegree--nd000"
+		}
+	],
+
+	"otherCourses" : [
+		{
+			"title" : "Presidio de Santa Barbara Archaeological Field School",
+			"school" : "California State University, San Luis Obispo",
+			"location" : "Santa Barbara, CA",
+			"dates" : "June 2011-July 2011",
+			"url" : "http://www.sbthp.org/#!field-school/cu4k"
+		}
+	]
+}
+
+/*
+For education.schools, education.onlineCourses, and education.otherCourses,
+formats data from each hash to its respective helper.js string and appends
+to the education-entry class
+*/
+
+education.display = function()
+{
+	for (school in education.schools)
+	{
+		$("#education").append(HTMLschoolStart);
+		var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		var formattedSchoolMajors = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		var formattedSchoolURL = HTMLschoolURL.replace(/%data%/g, education.schools[school].url);
+		$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree + formattedSchoolDates + formattedSchoolLocation + formattedSchoolMajors + formattedSchoolURL);
+	}
+
+	$("#education").append(HTMLonlineClasses);
+	for (course in education.onlineCourses)
+	{
+		$("#education").append(HTMLschoolStart);
+		var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[course].title);
+		var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[course].school);
+		var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
+		var formattedOnlineURL = HTMLonlineURL.replace(/%data%/g, education.onlineCourses[course].url);
+		$(".education-entry:last").append(formattedOnlineSchool + formattedOnlineDates + formattedOnlineTitle + formattedOnlineURL);
+	}
+
+	$("#education").append(HTMLotherClasses);
+
+	for (course in education.otherCourses)
+	{
+		$("#education").append(HTMLschoolStart);
+		var formattedOtherTitle = HTMLotherTitle.replace("%data%", education.otherCourses[course].title);
+		var formattedOtherSchool = HTMLotherSchool.replace("%data%", education.otherCourses[course].school);
+		var formattedOtherLocation = HTMLotherLocation.replace("%data%", education.otherCourses[course].location);
+		var formattedOtherDates = HTMLotherDates.replace("%data%", education.otherCourses[course].dates);
+		var formattedOtherURL = HTMLotherURL.replace(/%data%/g, education.otherCourses[course].url);
+		$(".education-entry:last").append(formattedOtherSchool + formattedOtherDates + formattedOtherLocation + formattedOtherTitle + formattedOtherURL);
+	}
+}
+
+education.display();
+
+$("#mapDiv").append(googleMap);
